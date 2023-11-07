86410522b917b7835cfbe3c564e120b6e5c1b0a3
Atio <atiowahyudi02@gmail.com>
2023-11-07 18:52:58 +0700

    fix bug

--- src/router/Recipe.js
+++ src/router/Recipe.js
@@ -20,7 +20,7 @@ router.get('/search', Protect, getSearchData)
 router.get('/recipe', Protect, getData);
 router.get('/recipe/detail', getDataDetail);
 router.get('/recipe/:id', getDataById);
-router.get('/data',getUsersRecipe, Protect)
+router.get('/data/:id',getUsersRecipe, Protect)
 router.post('/recipe', Protect,upload.single('image'), postData);
 router.put('/recipe/:id', Protect,upload.single('image'), putData);
 router.delete('/recipe/:id', Protect, deleteData);
